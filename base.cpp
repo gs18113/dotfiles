3da6ea739f4f92051d1911d54e9a032b3449893b
Seungmin Choi <seungmin960@gmail.com>
2021-01-11 01:50:57 +0900

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define eb emplace_back
+#define pb push_back
+#define fi first
+#define se second
+#define all(v) (v).begin(), (v).end()
+
+using namespace std;
+typedef long long int ll;
+typedef pair<int, int> pi;
+typedef tuple<int, int, int> ti;
+
+int main(){
+	
+	return 0;
+}
